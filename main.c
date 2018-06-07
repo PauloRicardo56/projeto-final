7cd0105d11537b8a36e90cf4757a1ea38dbd7b0b
Paulo Ricardo <praraujo.vieira@gmail.com>
2018-06-07 11:18:08 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,46 @@
 #include <stdio.h>
 
 int main() {
+    int resposta;
+    
+    do {
+        showMenu();
+        resposta = leValidaInt(1, 5, "Informe uma das opcoes do menu");
+        
+        switch(resposta) {
+            case '1':
+                menuDadosPiloto();
+                break;
+            case '2':
+                menuDadosEquipe();
+                break;
+            case '3':
+                menuDadosCircuito();
+                break;
+            case '4':
+                menuDadosVolta();
+                break;
+            case '5':
+        }
+    } while(resposta != 5);
+    
     return 0;
 }
+
+
+void showMenu() {}
+
+
+int leValidaInt() {}
+
+
+void menuDadosPiloto() {}
+
+
+void menuDadosEquipe() {}
+
+
+void menuDadosCircuito() {}
+
+
+void menuDadosVolta() {}
