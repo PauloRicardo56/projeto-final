411545e75ee53fa4a0e5e04929e26129b5689a25
Paulo Ricardo <praraujo.vieira@gmail.com>
2018-06-11 10:02:43 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
 
+
+struct Piloto {
+    int id;
+    char nome[30], siglaEquipe[4], dataNasc[11], sexo, paisOrigem[20];
+};
+
+
+struct Equipe {
+    char nome[40], sigla[4], paisOrigem[20];
+};
+
+
+struct Circuito {
+    int codigo, idPilotoMenorTempo;
+    char nome[30], pais[20];
+    float tamanho;
+    long int menorTempo; //Podemos fazer em milisegundos e na hora de apresentar converter para minutos e segundos.
+};
+
+
+struct MelhorVolta {};    
+
+
 int main() {
     int resposta;
     
@@ -28,19 +51,85 @@ int main() {
 }
 
 
-void showMenu() {}
+void showMenu() {
+    printf("1-Menu dos pilotos\n");
+    printf("2-Menu das equipes\n");
+    printf("3-Menu dos circuitos\n");
+    printf("4-Menu das voltas\n");
+}
 
 
-int leValidaInt() {}
+int leValidaInt() {} //Vai estar em validacoes.h
+
+/*
+PODEMOS COLOCAR CADA PARTE (PILOTO, EQUIPE, CIRCUITO E VOLTAS) EM ARQUIVOS SEPARADOS).
+*/
+
+void menuDadosPiloto() {
+    int resposta;
+    
+    showMenuPiloto();
+    resposta = leValidaInt(1, 3, "Digite uma das opcoes do menu");
+    switch(resposta) {
+        case 1:
+            cadastrarPiloto();
+            break;
+        case 2:
+            alterarPiloto();
+            break;
+        case 3:
+            removerPiloto();
+    }
+}
 
 
-void menuDadosPiloto() {}
+void showMenuPiloto() {
+    printf("1-Cadastrar piloto\n");
+    printf("2-Alterar piloto\n");
+    printf("3-Remover piloto\n");
+}
 
 
-void menuDadosEquipe() {}
+void menuDadosEquipe() {
+    int resposta;
+    
+    showMenuEquipe();
+    resposta = leValidaInt(1, 2, "Digite uma das opcoes do menu");
+    switch(resposta) {
+        case 1:
+            cadastrarEquipe();
+            break;
+        case 2:
+            excluitEquipe();
+    }
+}
 
 
-void menuDadosCircuito() {}
+void showMenuEquipe() {
+    printf("1-Cadastrar nova equipe\n");
+    printf("2-Excluir equipe\n");
+}
+
+
+void menuDadosCircuito() {
+    int resposta;
+    
+    showMenuCircuito();
+    resposta = leValidaInt(1, 2, "Digite uma das opcoes do menu");
+    switch(resposta) {
+        case 1:
+            cadastrarCircuito();
+            break;
+        case 2:
+            alterarCircuito();
+    }
+}
+
+
+void showMenuCircuito() {
+    printf("1-Cadastrar novo circuito\n");
+    printf("2-Alterar circuito\n");
+}
 
 
 void menuDadosVolta() {}
